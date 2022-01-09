fdc7de98bb1e7ff789346c25155902aa6522a888
ShahSau <shahriarksaurov@gmail.com>
2022-01-10 01:49:39 +0200

    update

--- nextjs-first/pages/index.js
+++ nextjs-first/pages/index.js
@@ -1,14 +1,30 @@
-import React from "react";
-import Link from "next/link";
-function HomePage() {
+import fs from "fs/promises";
+import path from "path";
+function HomePage(props) {
+  const { products } = props;
   return (
-    <div>
-      <h1>hii</h1>
-      <Link href="/portfolio">portfolio</Link>
-      <br />
-      <Link href="/clients">cleints</Link>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
 }
-
+//if nextjs see the getStaticProps, it will calll this before anything in the page
+export async function getStaticProps() {
+  //when next js will excuate this file it will trate this file
+  //as it was sitting in the root file.
+  //so process.cwd() will be root of the project
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data= JSON.parse(jsonData)
+  //always return an object
+  return {
+    props: {
+      products: data.products,
+    },
+    // after how many seconds it should be regenerated in production
+    revalidate:10
+  };
+}
 export default HomePage;
